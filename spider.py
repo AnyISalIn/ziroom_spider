b11d60f182d1c8bac9c9fc73a8a634d54913670c
AnyISalIn <anyisalin@gmail.com>
2017-02-21 10:45:41 +0800

    support house detail info

--- spider.py
+++ spider.py
@@ -11,6 +11,10 @@ class House:
         self.subway_distance = kwargs.get('subway_distance')
         self.area = kwargs.get('area')
         self.url = kwargs.get('url')
+        if kwargs.get('detail'):
+            self.id = kwargs['detail'].get('id')
+            self.environment = kwargs['detail'].get('environment')
+            self.traffic = kwargs['detail'].get('traffic')
 
     def __repr__(self):
         return '<House {} {} {}>'.format(self.location, self.price, self.area)
@@ -64,7 +68,26 @@ class ZiroomSpider:
     def __get_url(self, house):
         return 'http:' + house.a.attrs.get('href')
 
-    def __house_info(self, house):
+    def __get_id(self, house_bs, house_url):
+        return house_bs.find_all('h3')[0].text.replace('\n', '').split(' ')[1]
+
+    def __get_environment(self, house_bs, house_url):
+        return [e.text for e in house_bs.find_all('div', attrs={'class': 'aboutRoom gray-6'})[0].find_all('p') if '周边' in e.strong.text][0]
+
+    def __get_traffic(self, house_bs, house_url):
+        return [e.text for e in house_bs.find_all('div', attrs={'class': 'aboutRoom gray-6'})[0].find_all('p') if '交通' in e.strong.text][0]
+
+    def __get_detail(self, house_url):
+        data = requests.get(house_url).text
+        house_bs = bs(data, 'lxml')
+        detail_attr = {
+            'id': self.__get_id(house_bs, house_url),
+            'environment': self.__get_environment(house_bs, house_url),
+            'traffic': self.__get_traffic(house_bs, house_url)
+        }
+        return detail_attr
+
+    def __house_info(self, house, detail=False):
         attrs = {
             'location': self.__get_location(house),
             'price': self.__get_price(house),
@@ -73,13 +96,16 @@ class ZiroomSpider:
             'area': self.__get_area(house),
             'url': self.__get_url(house)
         }
+        if detail is True:
+            attrs['detail'] = self.__get_detail(attrs.get('url'))
+
         return House(**attrs)
 
-    def houses(self, all=False):
-        return [self.__house_info(h) for h in self.__houses(all)]
+    def houses(self, all=False, detail=False):
+        return [self.__house_info(h, detail) for h in self.__houses(all=all)]
 
-    def cheapest(self, all=False, number=1):
-        return heapq.nsmallest(number, self.houses(all), lambda h: h.price)
+    def cheapest(self, all=False, detail=False, number=1):
+        return heapq.nsmallest(number, self.houses(all=all, detail=detail), lambda h: h.price)
 
-    def max_area(self, all=False, number=1):
-        return heapq.nlargest(number, self.houses(all), lambda h: float(h.area.replace('㎡', '')))
+    def max_area(self, all=False, detail=False, number=1):
+        return heapq.nlargest(number, self.houses(all=all, detail=detail), lambda h: float(h.area.replace('㎡', '')))
